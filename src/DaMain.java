525609bf7d367aa206385d146572df6bdb1e8af1
dylward757 <dylward757@gmail.com>
2017-05-15 12:47:50 -0600

    Bug Fix

--- src/DaMain.java
+++ src/DaMain.java
@@ -4,11 +4,8 @@ public class DaMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Initial build");
-		//System.out.println("Working on a bug fix");
-		//System.out.println("YAY bug is fixed");
-		//System.out.println("Trying a new GUI");
-		//System.out.println("New GUI works");
-		//
+		System.out.println("Working on a bug fix");
+		System.out.println("YAY bug is fixed");
 		
 	}
 
